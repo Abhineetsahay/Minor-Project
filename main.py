9025044cf30f36412fcb09324a6a509eea477ace
Abhineetsahay <abhineetsahay@gmail.com>
2026-02-24 20:54:10 +0530

    render html filer

--- main.py
+++ main.py
@@ -1,31 +1,37 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import pickle
 import pandas as pd
-from flask_cors import CORS 
+from flask_cors import CORS
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 CORS(app)
-# Load trained model
-with open("cardio_model.pkl", "rb") as f:
-    data = pickle.load(f)
 
-model = data["model"]
-columns = data["columns"]
+model = None
+columns = None
 
+def load_model():
+    global model, columns
+    if model is None:
+        model_path = os.path.join(os.getcwd(), "cardio_model.pkl")
+        with open(model_path, "rb") as f:
+            data = pickle.load(f)
+        model = data["model"]
+        columns = data["columns"]
 
 @app.route("/")
 def home():
-    return "Cardio Prediction API Running"
+    return render_template("index.html")  
 
 @app.route("/predict", methods=["POST"])
 def predict():
+    load_model()
+    
     input_data = request.get_json()
 
-    # Convert input to dataframe
     df = pd.DataFrame([input_data])
     df['BMI'] = df['weight'] / (df['height'] / 100) ** 2
 
-    # Ensure correct column order
     df = df[columns]
 
     # Prediction
@@ -38,6 +44,6 @@ def predict():
         "result": "Cardiovascular Disease Detected" if prediction == 1 else "Healthy (No Cardio Disease)"
     })
 
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
